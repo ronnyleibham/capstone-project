b7143546e4b02c556774d36cd7ecdd1429d63ae3
Ronny Leibham <ronnyleibham@gmx.de>
2021-11-28 14:45:22 +0100

    Add Font CocoGoose Regular & Light

--- src/app/GlobalStyles.tsx
+++ src/app/GlobalStyles.tsx
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
+import CocoGooseRegular from '../assets/fonts/Cocogoose-Pro-Regular.ttf';
+import CocoGooseLight from '../assets/fonts/Cocogoose-Pro-Light.ttf';
 
 const GlobalStyles = createGlobalStyle`
     *,*::before,*::after {
@@ -12,10 +14,20 @@ const GlobalStyles = createGlobalStyle`
      --yellow-dark: #f3b90e;
     --grey-dark: #231f20;    }
 
+    @font-face {
+        font-family: CocoGooseRegular;
+        src: url(${CocoGooseRegular});
+    
+    }
+    @font-face {
+        font-family: CocoGooseLight;
+        src: url(${CocoGooseLight});
+    }
+
     body {
   margin: 0;
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'CocoGooseRegular', 'CocoGooseLight',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
