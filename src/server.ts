3a839c65ab85627fe8bae9824f2fc4533a1affd5
Ronny Leibham <ronnyleibham@gmx.de>
2021-12-14 10:31:44 +0100

    Setup Rest Api and server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+
+// imports from 'node-fetch'
 import fetch from 'node-fetch';
+import type { RequestInit } from 'node-fetch';
+
+// imports from 'form-data'
+import FormData from 'form-data';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -15,31 +23,62 @@ app.get('/api/hello', (_request, response) => {
 
 app.use('/storybook', express.static('dist/storybook'));
 
+// upload image to storage and receive storageID
+app.post('/api/getStorageId/', async (_req, res) => {
+  const imageURL = '/Users/ronny/Desktop/maverick.jpg'; //change to: req.params
+  const exampleFile = fs.createReadStream(path.join(__dirname, `${imageURL}`));
+
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'multipart/form-data');
+  myHeaders.append('Authorization', 'Key {{$dotenv DEEPVA_API_KEY}}');
+
+  const form = new FormData();
+  form.append('folder', '/');
+  form.append('file', exampleFile);
+
+  const options: RequestInit = {
+    method: 'POST',
+    headers: myHeaders,
+    body: form,
+    redirect: 'follow',
+  };
+
+  const response = await fetch(
+    `${process.env.DEEPVA_BASE_URL}/storage/`,
+    options
+  );
+  const serverRes = await response.json();
+  res.send(serverRes);
+  console.log(serverRes);
+});
+
+// start job with chosen modules and receive JobID
 app.post('/api/getJobId/', async (_req, res) => {
-  const storageId = 'storage://zzaQ3pIxO9Nb9hMHWpfD'; //change to: req.params
+  const storageId = 'storage://mC2hvpBFNy1SnO0rNBUh'; //change to: req.params
   const options = {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       Authorization: `Key ${process.env.DEEPVA_API_KEY}`,
     },
-    body: {
+    body: JSON.stringify({
       sources: [`${storageId}`],
       modules: {
         face_recognition: {
           model: 'celebrities',
         },
       },
-    },
+    }),
   };
-  const response = await fetch(`${process.env.DEEPVA_BASE_URL},${options}`);
+  const response = await fetch(`${process.env.DEEPVA_BASE_URL}/jobs/`, options);
   const serverRes = await response.json();
   res.send(serverRes);
   console.log(serverRes);
 });
 
-app.get('/api/jobs/:id', async (_req, res) => {
-  const jobId = '94367e42-2c07-4be3-aed7-e3021692e058'; //change to: req.params
+// call done job from API with JobID
+app.get('/api/jobs/', async (_req, res) => {
+  const jobId = '0578aef7-bebc-434c-927f-6043d1741a13'; //change to: req.params
   const options = {
     method: 'GET',
     headers: {
@@ -52,7 +91,6 @@ app.get('/api/jobs/:id', async (_req, res) => {
     options
   );
   const serverRes = await response.json();
-
   res.send(serverRes);
 });
 
